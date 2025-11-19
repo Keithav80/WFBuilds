f7aa04c52bd0b2e9580a4aa33935e017dc47aa7e
Keithav80 <keithav@protonmail.com>
2025-11-19 16:29:23 +0000

    Initial upload

---
+++ wffan.js
@@ -0,0 +1,31 @@
+function countdown(date, elementId) {
+  let countDownDate = new Date(date).getTime();
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById(elementId).innerHTML = `<span>${days}</span> D, <span>${hours}</span>h, <span>${minutes}</span>m`;
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById(elementId).innerHTML = 'LIVE!';
+    }
+  }, 1000);
+}
+
+countdown('2025-11-19, 19:00:00', 'Resurgence'); // Resurgence Rotation
+countdown('2025-12-01, 16:00:00', 'IGE'); // In Game Events
+countdown('2025-10-26, 23:00:00', 'Nightwave'); // New Nightwave
+
+countdown('2025-11-25, 19:00:00', 'Devshort'); // Tuesdays
+countdown('2025-11-19, 20:00:00', 'WF-int'); // Wednesdays
+countdown('2025-11-20, 23:00:00', 'PrimeTime'); // Thursdays
+countdown('2025-11-28, 19:00:00', 'Devstream'); // Last Friday of Month
+
+countdown('2025-12-17, 16:00:00', 'WF-prime'); // Prime Warframe release
+countdown('2025-12-17, 16:00:00', 'Update'); // Main & Interim Updates
+
