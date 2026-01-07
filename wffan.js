2518a9e5e057e1548ca887ba3cca5c091eec36f3
Keithav80 <keithav@protonmail.com>
2026-01-07 11:05:46 +0000

    Update wffan.js

--- wffan.js
+++ wffan.js
@@ -25,8 +25,8 @@ countdown('2025-12-01, 16:00:00', 'IGE1'); // In Game Event 1
 countdown('2025-12-16, 16:00:00', 'IGE2'); // In Game Event 2
 countdown('2025-10-26, 23:00:00', 'Nightwave'); // Nightwave
 
-countdown('2025-12-16, 19:00:00', 'Devshort'); // Tuesdays
-countdown('2025-12-17, 20:00:00', 'WF-int'); // Wednesdays
-countdown('2025-12-18, 23:00:00', 'PrimeTime'); // Thursdays
-countdown('2026-01-30, 19:00:00', 'Devstream'); // Last Friday of Month
+countdown('2026-01-13, 19:00:00', 'Devshort'); // Tuesdays
+countdown('2026-01-14, 20:00:00', 'WF-int'); // Wednesdays
+countdown('2026-01-08, 23:00:00', 'PrimeTime'); // Thursdays
+countdown('2026-01-23, 19:00:00', 'Devstream'); // Last Friday of Month
 
