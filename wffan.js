94b5dc732453f7da541a45003bb027cc0d796fd2
Keithav80 <keithav@protonmail.com>
2026-01-18 17:31:18 +0000

    Add files via upload

--- wffan.js
+++ wffan.js
@@ -20,15 +20,13 @@ function countdown(date, elementId) {
 countdown('2025-12-10, 16:00:00', 'WF-prime'); // Prime Warframe release
 countdown('2025-12-10, 16:00:00', 'Update'); // Main & Interim Updates
 
-countdown('2025-12-17, 19:00:00', 'Resurgenc'); // Resurgence Rotation
-countdown('2026-12-01, 16:00:00', 'IGE1'); // Annual Events
+countdown('2026-02-12, 19:00:00', 'Resurgence'); // Resurgence Rotation
+countdown('2025-12-01, 16:00:00', 'IGE1'); // Annual Events
 countdown('2025-12-16, 16:00:00', 'IGE2'); // Event 2 (if multiple)
 countdown('2025-10-26, 23:00:00', 'Nightwave'); // Nightwave
 
-countdown('2026-01-13, 19:00:00', 'Devshort'); // Tuesdays
-countdown('2026-01-14, 20:00:00', 'WF-int'); // Wednesdays
-countdown('2026-01-15, 23:00:00', 'PrimeTime'); // Thursdays
+countdown('2026-01-20, 19:00:00', 'Devshort'); // Tuesdays
+countdown('2026-01-21, 20:00:00', 'WF-int'); // Wednesdays
+countdown('2026-01-22, 23:00:00', 'PrimeTime'); // Thursdays
 countdown('2026-01-23, 19:00:00', 'Devstream'); // Last Friday of Month
 
-
-// line 23 'e' missing, temporary deliberate fault
