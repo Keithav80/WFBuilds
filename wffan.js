6ddd4dde8f026dd3a7ffb87523f5dd29bf92e753
Keithav80 <keithav@protonmail.com>
2026-02-06 16:13:56 +0000

    Add files via upload

--- wffan.js
+++ wffan.js
@@ -8,7 +8,7 @@ function countdown(date, elementId) {
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById(elementId).innerHTML = `<span>${days}</span> D: <span>${hours}</span>h: <span>${minutes}</span>m`;
+    document.getElementById(elementId).innerHTML = `<span>${days}D:</span> <span>${hours}h:</span> <span>${minutes}m</span>`;
 
     if (distance < 0) {
       clearInterval(x);
@@ -21,14 +21,13 @@ countdown('2026-03-11, 16:00:00', 'WF-prime'); // Prime Warframe release
 countdown('2026-03-11, 16:00:00', 'Update'); // Main & Interim Updates
 
 countdown('2026-02-12, 19:00:00', 'Resurgence'); // Resurgence Rotation
-countdown('2026-02-04, 16:00:00', 'IGE1'); // Annual Events
+countdown('2026-02-01, 16:00:00', 'IGE1'); // Annual Events
 countdown('2025-12-16, 16:00:00', 'IGE2'); // Event 2 (if multiple)
 countdown('2025-10-26, 23:00:00', 'Nightwave'); // Nightwave
 
-countdown('2026-02-03, 19:00:00', 'Devshort'); // Tuesdays
-countdown('2026-02-04, 20:00:00', 'WF-int'); // Wednesdays
-countdown('2026-02-05, 23:00:00', 'PrimeTime'); // Thursdays
+countdown('2026-02-10, 19:00:00', 'Devshort'); // Tuesdays
+countdown('2026-02-11, 20:00:00', 'WF-int'); // Wednesdays
+countdown('2026-02-12, 23:00:00', 'PrimeTime'); // Thursdays
 countdown('2026-02-27, 19:00:00', 'Devstream'); // Last Friday of Month
-
-
+countdown('2026-07-11, 16:00:00', 'TennoCon'); //Annual Event
 
